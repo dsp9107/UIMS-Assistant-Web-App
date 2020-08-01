1cef9e99b2332d362f30e70c9d7bd6e2006dfc47
Dhawal Singh Panwar <dsp9107@outlook.com>
2020-08-01 17:30:18 +0530

    integrated firebase performance

--- public/scripts/custom-analytics.js
+++ public/scripts/custom-analytics.js
@@ -1,18 +1,3 @@
-// INITIALIZE
-
-var firebaseConfig = {
-    apiKey: "AIzaSyARepvtLqm28z8JLo58YBWdBIdcyWsTJfY",
-    authDomain: "uims-assistant.firebaseapp.com",
-    databaseURL: "https://uims-assistant.firebaseio.com",
-    projectId: "uims-assistant",
-    storageBucket: "uims-assistant.appspot.com",
-    messagingSenderId: "190486161620",
-    appId: "1:190486161620:web:b511dcaf2a4a0c172df341",
-    measurementId: "G-3806LQMTMS",
-};
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 // EVENTS - Navbar
 
 $(".brand-logo").click(() => {
